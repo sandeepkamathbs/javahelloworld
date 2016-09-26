ba9874980e58ba90a312a1fd417d53ad235eb193
sandeep.a.kamath <sandeep.a.kamath@192.168.56.1>
2016-09-26 16:21:34 +1000

    init commit

---
+++ src/javahelloworld/helloworld.java
@@ -0,0 +1,11 @@
+package javahelloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wass up sandeep");
+
+	}
+
+}
