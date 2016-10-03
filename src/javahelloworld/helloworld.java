7bc04e2526bc1e287145e84f299ba29a0684ca70
sandeep.a.kamath <sandeep.a.kamath@192.168.56.1>
2016-10-03 16:41:21 +1100

    test

--- src/javahelloworld/helloworld.java
+++ src/javahelloworld/helloworld.java
@@ -5,7 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wass up sandeep");
-
+		System.out.println("Hello world");
 	}
 
 }
